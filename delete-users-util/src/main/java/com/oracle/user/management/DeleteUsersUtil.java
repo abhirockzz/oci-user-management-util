6cbf2c9f7b394538126cf26645b2ed1f7fc2758d
Abhishek <abhishek.af.gupta@oracle.com>
2018-09-02 23:19:31 +0530

    factors in 'start_user_sequence' and 'num_of_users' for deletion

--- delete-users-util/src/main/java/com/oracle/user/management/DeleteUsersUtil.java
+++ delete-users-util/src/main/java/com/oracle/user/management/DeleteUsersUtil.java
@@ -7,12 +7,14 @@ import com.oracle.bmc.auth.SimpleAuthenticationDetailsProvider;
 import com.oracle.bmc.auth.SimplePrivateKeySupplier;
 import com.oracle.bmc.identity.IdentityClient;
 import com.oracle.bmc.identity.model.User;
+import com.oracle.bmc.identity.model.UserGroupMembership;
 import com.oracle.bmc.identity.requests.DeleteUserRequest;
 import com.oracle.bmc.identity.requests.ListUserGroupMembershipsRequest;
 import com.oracle.bmc.identity.requests.ListUsersRequest;
 import com.oracle.bmc.identity.requests.RemoveUserFromGroupRequest;
 
 import java.io.FileInputStream;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 import java.util.Scanner;
@@ -41,6 +43,12 @@ public class DeleteUsersUtil {
         String usernamePrefix = props.getProperty("username_prefix");
         System.out.println("Username prefix " + usernamePrefix);
 
+        int startUserSequence = Integer.valueOf(props.getProperty("start_user_sequence"));
+        System.out.println("Start user sequence " + startUserSequence);
+
+        int numOfUsers = Integer.valueOf(props.getProperty("num_of_users"));
+        System.out.println("Number of users " + numOfUsers);
+
         ConfigFileReader.ConfigFile config = ConfigFileReader.parse(configFile, "DEFAULT");
 
         AuthenticationDetailsProvider provider
@@ -54,8 +62,8 @@ public class DeleteUsersUtil {
 
         IdentityClient identityClient = new IdentityClient(provider);
         identityClient.setRegion(config.get("region"));
-        
-       /**
+
+        /**
          * compartment ID required by the API is nothing but the root
          * compartment OCID (same as tenancy OCID) see -
          * https://docs.cloud.oracle.com/iaas/tools/java/latest/com/oracle/bmc/identity/model/CreateUserDetails.html#getCompartmentId--
@@ -65,11 +73,21 @@ public class DeleteUsersUtil {
 
         ListUsersRequest listUsersRequest = ListUsersRequest.builder().compartmentId(compartmentOCID).limit(10000).build();
         List<User> allUsers = identityClient.listUsers(listUsersRequest).getItems();
+        //List<String> allUsernames = allUsers.stream().map((u) -> u.getName()).collect(Collectors.toList());
 
-        List<User> usersToBeDeleted = allUsers.stream()
-                .filter((u) -> u.getName().startsWith(usernamePrefix))
-                .collect(Collectors.toList());
+        List<User> usersToBeDeleted = new ArrayList<>();
 
+        for (int i = startUserSequence; i < (numOfUsers + startUserSequence); i++) {
+            String filter = usernamePrefix + i;
+            usersToBeDeleted.addAll(allUsers.stream()
+                    .filter((u) -> u.getName().equalsIgnoreCase(filter))
+                    .collect(Collectors.toList()));
+        }
+
+        if (usersToBeDeleted.isEmpty()) {
+            System.out.println("None of the users matched deletion criteria. Exiting....");
+            return;
+        }
         System.out.println("Users to be DELETED....");
         usersToBeDeleted.stream().forEach((u) -> System.out.println("User " + u.getName()));
 
@@ -90,19 +108,27 @@ public class DeleteUsersUtil {
         for (String userOCID : usersOCIDsToBeDeleted) {
 
             try {
-
-                String userGroupMembershipId = identityClient.listUserGroupMemberships(ListUserGroupMembershipsRequest.builder()
+                List<UserGroupMembership> userGroupMembership = identityClient.listUserGroupMemberships(ListUserGroupMembershipsRequest.builder()
                         .compartmentId(compartmentOCID)
                         .userId(userOCID)
                         .groupId(groupOCID)
                         .build())
-                        .getItems()
-                        .get(0) //we just need the first one
-                        .getId();
+                        .getItems();
 
-                identityClient.removeUserFromGroup(RemoveUserFromGroupRequest.builder().userGroupMembershipId(userGroupMembershipId).build());
-                System.out.println("Deleted user from group");
+                String userGroupMembershipId = null;
+                if (!userGroupMembership.isEmpty()) {
+                    userGroupMembershipId = userGroupMembership.get(0) //ASSUMING user is part of ONE group ONLY
+                            .getId();
+                    identityClient.removeUserFromGroup(RemoveUserFromGroupRequest.builder().userGroupMembershipId(userGroupMembershipId).build());
+                    System.out.println("Deleted user from group");
 
+                }
+            } catch (Exception e) {
+                System.out.println("could not delete user " + userOCID + " from group due to " + e.getMessage());
+                //let delete user get triggered.. if group relationship exists, it is bound to fail...
+            }
+
+            try {
                 DeleteUserRequest deleteUserRequest = DeleteUserRequest.builder().userId(userOCID).build();
                 identityClient.deleteUser(deleteUserRequest);
                 System.out.println("Deleted user " + userOCID);
